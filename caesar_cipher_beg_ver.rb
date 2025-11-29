24122b7936b8b6a9f8c3099f9df3fdbc9a6d2fbf
Makara Pen <129983771+makpen26@users.noreply.github.com>
2025-11-29 17:42:01 -0600

    restructuring caesar_decipher.rb to use ternary operator but my usage did not follow the recommended usage of ternary, not for action

--- caesar_cipher_beg_ver.rb
+++ caesar_cipher_beg_ver.rb
@@ -1,25 +1,11 @@
 def caesar_cipher(string, shift_key)
-  valid_lower_case_alphabet = ('a'.ord..'z'.ord).to_a
-  valid_upper_case_alphabet = ('A'.ord..'Z'.ord).to_a
   ciphered_array = []
-  result_from_sum_char_with_shift_key = 0
 
   string.bytes.each do |ord_char|
-    if valid_lower_case_alphabet.include?(ord_char)
-      result_from_sum_char_with_shift_key = ord_char + shift_key
-      ciphered_array << if result_from_sum_char_with_shift_key > 'z'.ord
-                          # valid_lower_case_alphabet[result_from_sum_char_with_shift_key - 'z'.ord - 1].chr
-                          valid_lower_case_alphabet[(result_from_sum_char_with_shift_key - 'a'.ord) % 26].chr
-                        else
-                          result_from_sum_char_with_shift_key.chr
-                        end
-    elsif valid_upper_case_alphabet.include?(ord_char)
-      result_from_sum_char_with_shift_key = ord_char + shift_key
-      ciphered_array << if result_from_sum_char_with_shift_key > 'Z'.ord
-                          valid_upper_case_alphabet[(result_from_sum_char_with_shift_key - 'A'.ord) % 26].chr
-                        else
-                          result_from_sum_char_with_shift_key.chr
-                        end
+    if ('a'.ord..'z'.ord).to_a.include?(ord_char)
+      ciphered_array << (ord_char + shift_key > 'z'.ord ? ('a'.ord..'z'.ord).to_a[(ord_char + shift_key - 'a'.ord) % 26].chr : (ord_char + shift_key).chr)
+    elsif ('A'.ord..'Z'.ord).to_a.include?(ord_char)
+      ciphered_array << (ord_char + shift_key > 'Z'.ord ? ('A'.ord..'Z'.ord).to_a[(ord_char + shift_key - 'A'.ord) % 26].chr : (ord_char + shift_key).chr)
     else
       ciphered_array << ord_char.chr
     end
@@ -28,6 +14,4 @@ def caesar_cipher(string, shift_key)
   p ciphered_array.join
 end
 
-caesar_cipher('what a string!', 5)
 caesar_cipher('What a string!', 5)
-

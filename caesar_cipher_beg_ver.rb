581fe3667ac5c3ef3ab9485f474ca86417cbe53f
Makara Pen <129983771+makpen26@users.noreply.github.com>
2025-11-29 18:13:48 -0600

    now readjust the ternary operator to pick which value instead of doing actions

--- caesar_cipher_beg_ver.rb
+++ caesar_cipher_beg_ver.rb
@@ -2,16 +2,20 @@ def caesar_cipher(string, shift_key)
   ciphered_array = []
 
   string.bytes.each do |ord_char|
-    if ('a'.ord..'z'.ord).to_a.include?(ord_char)
-      ciphered_array << (ord_char + shift_key > 'z'.ord ? ('a'.ord..'z'.ord).to_a[(ord_char + shift_key - 'a'.ord) % 26].chr : (ord_char + shift_key).chr)
-    elsif ('A'.ord..'Z'.ord).to_a.include?(ord_char)
-      ciphered_array << (ord_char + shift_key > 'Z'.ord ? ('A'.ord..'Z'.ord).to_a[(ord_char + shift_key - 'A'.ord) % 26].chr : (ord_char + shift_key).chr)
-    else
-      ciphered_array << ord_char.chr
-    end
+    ciphered_array << if ('a'.ord..'z'.ord).to_a.include?(ord_char)
+                        helper_method_to_shift_key(ord_char, shift_key, 'a'.ord, 'z'.ord)
+                      elsif ('A'.ord..'Z'.ord).to_a.include?(ord_char)
+                        helper_method_to_shift_key(ord_char, shift_key, 'A'.ord, 'Z'.ord)
+                      else
+                        ord_char.chr
+                      end
   end
 
   p ciphered_array.join
+end
+
+def helper_method_to_shift_key(ord_char, shift_key, first_char, last_char)
+  ord_char + shift_key > last_char ? (((ord_char + shift_key - first_char) % 26) + first_char).chr : (ord_char + shift_key).chr
 end
 
 caesar_cipher('What a string!', 5)
